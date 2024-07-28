2d49d6ce5dcb9bd0d17657bf2012784a10f81214
zzhui1976 <zzhzjy@hotmail.com>
2024-07-28 12:45:16 +0800

    modify some

--- src/app/business/view002/_mock.ts
+++ src/app/business/view002/_mock.ts
@@ -2,19 +2,28 @@ import { MockRequest } from '@delon/mock';
 
 const fundNames = [
   '无风险3年定存',
-  '沪深300',
-  '其他ETF基金1',
-  '其他ETF基金2',
-  '其他ETF基金3',
-  '其他ETF基金4',
-  '其他ETF基金5',
-  '其他ETF基金6',
-  '其他ETF基金7',
-  '其他ETF基金8',
-  '其他ETF基金9'
+  '沪深300ETF',
+  '新能源ETF',
+  '智能出行ETF',
+  '消费行业ETF',
+  '中证100ETF',
+  '港股互联网ETF',
+  '港股房地产ETF',
+  '医药100ETF',
+  '信创概念ETF',
+  'TMT50ETF'
 ];
 
-const generateRandomData = (days: number) => {
+const trade_cal: string[] = [];
+for (let i = 100; i >= 0; i--) {
+  trade_cal.push(new Date(new Date().setDate(-i)).toLocaleDateString());
+}
+
+const generateRandomData = (from_date: string, to_date: string) => {
+  let from_index = trade_cal.indexOf(from_date) || 0;
+  let to_index = trade_cal.indexOf(to_date) || 100;
+  let days: number = to_index - from_index + 1;
+  let trade_cal_range = trade_cal.slice(from_index, to_index + 1);
   const data = {} as { [key: string]: number[] };
   fundNames.forEach(fund => {
     data[fund] = [0];
@@ -25,12 +34,16 @@ const generateRandomData = (days: number) => {
       data[fund].push(newReturn);
     }
   });
-  return data;
+  return { data, trade_cal_range };
 };
 
 export const FUNDS = {
   '/view002/funds': (req: MockRequest) => {
-    const days = parseInt(req.queryString.days, 10) || 30;
-    return generateRandomData(days);
+    const from_date = req.queryString.from_date;
+    const to_date = req.queryString.to_date;
+    return generateRandomData(from_date, to_date);
+  },
+  '/view002/trade_cal': (req: MockRequest) => {
+    return { trade_cal };
   }
 };
--- src/app/business/view002/fund-chart.component.ts
+++ src/app/business/view002/fund-chart.component.ts
@@ -12,7 +12,14 @@ import { NzMarks } from 'ng-zorro-antd/slider';
     <nz-card [nzBordered]="false" nzTitle="投资收益率分析">
       <div nz-row>
         <div nz-col nzSpan="24">
-          <nz-slider [nzMarks]="marks" nzRange [nzMin]="-100" [nzMax]="0" [(ngModel)]="days" (nzOnAfterChange)="onDaysChange($event)" />
+          <nz-slider
+            [nzMarks]="marks"
+            nzRange
+            [nzMin]="-slider_len + 1"
+            [nzMax]="0"
+            [(ngModel)]="days"
+            (nzOnAfterChange)="onDaysChange($event)"
+          />
         </div>
       </div>
       <br />
@@ -28,26 +35,42 @@ import { NzMarks } from 'ng-zorro-antd/slider';
 export class FundChartComponent implements OnInit {
   chartOption: EChartsOption = {};
 
-  today = new Date().toLocaleDateString();
-  fromday = new Date(new Date().setDate(-100)).toLocaleDateString();
-
-  marks: NzMarks = { '-100': this.fromday, '0': this.today, '-30': new Date(new Date().setDate(-30)).toLocaleDateString() };
+  marks: NzMarks = {};
 
   days = [-30, 0];
+  slider_len = 101;
+  trade_cal = [];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.fetchData();
+    this.fetchTrade_cal();
   }
 
   fetchData(): void {
-    this.http.get(`/view002/funds?days=${this.days[1] - this.days[0]}`).subscribe((data: any) => {
-      this.updateChart(data);
+    let from_date = this.trade_cal[this.trade_cal.length - 1 + this.days[0]];
+    let to_date = this.trade_cal[this.trade_cal.length - 1 + this.days[1]];
+
+    this.http.get(`/view002/funds?from_date=${from_date}&to_date=${to_date}`).subscribe((result: any) => {
+      this.updateChart(result.data, result.trade_cal_range);
     });
   }
 
-  updateChart(data: any): void {
+  fetchTrade_cal(): void {
+    this.http.get(`/view002/trade_cal`).subscribe((result: any) => {
+      this.trade_cal = result.trade_cal;
+      this.slider_len = this.trade_cal.length;
+      this.marks = {};
+      let marks_index: number[] = [0, -this.trade_cal.length + 1, this.days[0], this.days[1]];
+      for (let index of marks_index) {
+        this.marks[`${index}`] = this.trade_cal[this.trade_cal.length - 1 + index];
+      }
+      console.log('ok');
+      this.fetchData();
+    });
+  }
+
+  updateChart(data: any, trade_cal_range: any): void {
     const series = [];
     for (const fund in data) {
       if (data.hasOwnProperty(fund)) {
@@ -68,9 +91,7 @@ export class FundChartComponent implements OnInit {
       },
       xAxis: {
         type: 'category',
-        data: Array.from({ length: this.days[1] - this.days[0] }, (_, i) =>
-          new Date(new Date().setDate(this.days[0] + i)).toLocaleDateString()
-        )
+        data: trade_cal_range as string[]
       },
       yAxis: {
         type: 'value',
@@ -84,13 +105,11 @@ export class FundChartComponent implements OnInit {
 
   onDaysChange(days: number[] | number): void {
     //this.days = days;
-
-    this.marks = {
-      '-100': this.fromday,
-      '0': this.today
-    };
-    this.marks[`${this.days[0]}`] = new Date(new Date().setDate(this.days[0])).toLocaleDateString();
-    this.marks[`${this.days[1]}`] = new Date(new Date().setDate(this.days[1])).toLocaleDateString();
+    this.marks = {};
+    let marks_index: number[] = [0, -this.trade_cal.length + 1, this.days[0], this.days[1]];
+    for (let index of marks_index) {
+      this.marks[`${index}`] = this.trade_cal[this.trade_cal.length - 1 + index];
+    }
 
     console.log(`onChange: ${this.days[1] - this.days[0]}`);
     this.fetchData();
